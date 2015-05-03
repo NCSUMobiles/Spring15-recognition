9474d7fe079729fc9079fe9b6480bfb601dd7911
Xavier Primus <xavierprimus@Xaviers-MacBook-Pro.local>
2015-05-03 04:34:16 -0400

    clean up font size

--- src/com/istudy/fragment/CountdownFragment.java
+++ src/com/istudy/fragment/CountdownFragment.java
@@ -5,6 +5,7 @@ import com.example.istudy.R;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +32,12 @@ public class CountdownFragment extends Fragment {
 		timeleft--;
 		
 		switch(timeleft){
+		case 6: 
+			tView.setTextColor(Color.rgb(238,200,35));
+			break;
+		case 5: 
+			tView.setTextColor(Color.rgb(238,170,35));
+			break;
 		case 4: 
 			tView.setTextColor(Color.rgb(238,130,35));
 			break;
@@ -45,7 +52,7 @@ public class CountdownFragment extends Fragment {
 			break;
 		}
 		
-		if(timeleft < 5 && timeleft > 0){
+		if(timeleft < 7 && timeleft > 0){
 			tView.setText(""+(timeleft - 1));
 		}
 		
